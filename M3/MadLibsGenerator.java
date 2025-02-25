e0ea416d15d28e08d363cec3d4e42ecded843b08
AndyCrespo <aac97@njit.edu>
2025-02-24 23:31:41 -0500

    M3 Hw 3

--- M3/MadLibsGenerator.java
+++ M3/MadLibsGenerator.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Random;
 
 /*
 Challenge 3: Mad Libs Generator (Randomized Stories)
@@ -18,7 +19,7 @@ Challenge 3: Mad Libs Generator (Randomized Stories)
 
 public class MadLibsGenerator extends BaseClass {
     private static final String STORIES_FOLDER = "M3/stories";
-    private static String ucid = "mt85"; // <-- change to your ucid
+    private static String ucid = "Aac97"; // <-- change to your ucid
 
     public static void main(String[] args) {
         printHeader(ucid, 3,
@@ -35,7 +36,57 @@ public class MadLibsGenerator extends BaseClass {
         }
         List<String> lines = new ArrayList<>();
         // Start edits
+        
+        File[] files = folder.listFiles();
+        Random random = new Random();
+        File chosenFile = files[random.nextInt(files.length)];
 
+        try (Scanner fileScanner = new Scanner(chosenFile)) {
+            while (fileScanner.hasNextLine()) {
+                lines.add(fileScanner.nextLine());
+            }
+        } catch (Exception e) {
+            System.out.println("Error reading file: " );
+            printFooter(ucid, 3);
+            scanner.close();
+            return;
+        }
+
+        for (int i = 0; i < lines.size(); i++) {
+            String line = lines.get(i);
+
+            int startIndex = line.indexOf('<');
+            while (startIndex != -1) {
+                int endIndex = line.indexOf('>', startIndex);
+
+
+                if (endIndex == -1) {
+                    break;
+                }
+
+
+                String placeholder = line.substring(startIndex + 1, endIndex); // e.g., "adjective" or "my_sillyword"
+
+                // Convert underscores to spaces for the prompt
+                String promptPlaceholder = placeholder.replace("_", " ");
+
+                // Prompt user
+                System.out.print("Enter a " + promptPlaceholder + ": ");
+                String userInput = scanner.nextLine();
+
+                // Rebuild the line with the user's input replacing the placeholder
+                // Everything before < + user input + everything after >
+                line = line.substring(0, startIndex) + userInput + line.substring(endIndex + 1);
+
+                // Look for the next '<' in the updated line
+                startIndex = line.indexOf('<');
+            }
+
+            // Update the collection with the modified line
+            lines.set(i, line);
+        }
+
+        
         // load a random story file
 
         // parse the story lines
